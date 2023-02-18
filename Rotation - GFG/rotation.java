8a1565cbc5f6fdb31df9b4db91aa8d281cdbd3ab
Tarun Choudhary <57765697+tarunchoudhary1805@users.noreply.github.com>
2023-02-18 21:46:34 +0530

    Added solution - LeetHub

--- Rotation - GFG/rotation.java
+++ Rotation - GFG/rotation.java
@@ -34,10 +34,27 @@ class Solution {
         int ans = BinarySearch(arr);
         return ans;
     }
+    // public static int BinarySearch(int [] arr, int n){
+    //     int start = 0;
+    //     int end = n-1;
+    //     while(start<=end){
+    //         int mid = start+(end-start)/2;
+    //         int prev = (mid+n-1)%n;
+    //         int next = (mid+1)%n;
+    //         if(arr[mid] <= arr[prev] && arr[mid] <= arr[next]){
+    //             return mid;
+    //         }else if(arr[start] <= arr[mid]){
+    //             start = mid+1;
+    //         }else if(arr[mid] <= arr[end]){
+    //             end = mid-1;
+    //         }
+    //     }
+    //     return 0;
+    // }
      public static int BinarySearch(int [] arr){
         int start = 0;
         int end = arr.length-1;
-        int ans = -1;
+        // int ans = -1;
         while(start<=end){
            int mid = start + (end-start)/2;
            int next = (mid+1)%arr.length;
@@ -50,6 +67,6 @@ class Solution {
                start = mid+1;
            }
         }
-        return ans;
+        return 0;
     }
 }
