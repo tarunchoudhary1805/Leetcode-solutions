80bee812bf9fcff50cfa2cd5c38bd79fd11cbd63
Tarun Choudhary <57765697+tarunchoudhary1805@users.noreply.github.com>
2023-03-23 20:26:37 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (23.84%) - LeetHub

---
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean checkPowersOfThree(int n) {
+        if(n <= 1) return true;
+        if(n==2) return false;
+        if(n%3 == 2) return false;
+        return checkPowersOfThree(n/3);
+    }
+}
