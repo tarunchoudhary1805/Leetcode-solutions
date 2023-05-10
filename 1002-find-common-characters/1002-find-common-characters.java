7ce2b962d3ace3551fd4259250171c2ac874f4e1
Tarun Choudhary <57765697+tarunchoudhary1805@users.noreply.github.com>
2023-05-10 16:55:59 +0530

    Time: 24 ms (13.42%), Space: 43.3 MB (13.85%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -1,38 +1,33 @@
 class Solution {
     public List<String> commonChars(String[] words) {
-        HashMap<Character,Integer> hmin = new HashMap<>();
-        
-        for(int i=0;i< words[0].length() ; i++){
-            char c=words[0].charAt(i);
-            hmin.put(c,hmin.getOrDefault(c,0)+1);  
+        HashMap<Character,Integer> map = new HashMap();
+        for(int i=0;i<words[0].length();i++){
+            map.put(words[0].charAt(i),map.getOrDefault(words[0].charAt(i),0)+1);
         }
         for(int i=0;i<words.length;i++){
-           HashMap<Character,Integer> hm = new HashMap<>();
-           for(int j=0;j<words[i].length();j++){
-               if(hm.containsKey(words[i].charAt(j))){
-                   hm.put(words[i].charAt(j),hm.get(words[i].charAt(j))+1);
-               }else{
-                   hm.put(words[i].charAt(j),1);
-               }
-           }
-            for(Character x : hmin.keySet()){
+            HashMap<Character,Integer> hm = new HashMap();
+            for(int j=0;j<words[i].length();j++){
+              hm.put(words[i].charAt(j),hm.getOrDefault(words[i].charAt(j),0)+1);
+            }
+            
+            for(Character x : map.keySet()){
                 if(hm.containsKey(x)){
-                    hmin.put(x,Math.min(hmin.get(x),hm.get(x)));
+                    map.put(x,Math.min(hm.get(x),map.get(x)));
                 }else{
-                    hmin.put(x,0);
+                    map.put(x,0);
                 }
             }
         }
-        List<String> list = new ArrayList<>();
-        for(Character x:hmin.keySet()){
-            if(hmin.get(x) > 0){
-                for(int i=0;i<hmin.get(x);i++){
-                    list.add(x+"");
-                }
-            }
+        ArrayList<String> list = new ArrayList<>();
+        System.out.println(map);
+        for(Character x : map.keySet()){
+           if(map.get(x) > 0){
+               for(int i=0;i<map.get(x);i++){
+                   list.add(x+"");
+               }
+                
+           }
         }
-        System.out.println(hmin);
         return list;
-        
     }
 }
